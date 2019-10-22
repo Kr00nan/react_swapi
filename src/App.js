fdbc9056e56c51eacdd8c191264145c754fbd3da
Michael Kroon <kroonology@gmail.com>
2019-10-22 11:36:14 -0600

    import data via axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import { Container, Header } from "semantic-ui-react";
+import React, { useState, useEffect } from 'react';
+import NavBar from './components/NavBar';
+import { Header, Card, Container } from "semantic-ui-react";
+import Axios from 'axios';
 
 function App() {
+  const [people, setPeople] = useState([]);
+
+  // useEffect(() => {
+  //   Axios.get('https://swapi.co/api/people/')
+  //     .then(res => {
+  //       let data = res.data.results;
+  //       setPeople(data)
+  //     })
+  //     .catch(err => {
+  //       console.log('something went wrong here...')
+  //     });
+  // }, []);
+
+  const renderPeople = () => {
+    if (people.length <= 0)
+      return <h2>Error contacting SWAPI...</h2>
+    return people.map(person => (
+      <Card>
+        <Card.Content>
+          <Card.Header>{person.name}</Card.Header>
+          <Card.Meta>{person.birth_year}</Card.Meta>
+          <Card.Description>
+            {person.homeworld}
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    ))
+  }
+
   return (
     <Container>
-      <Header>Hello</Header>
+      <Header as="h1">Star Wars Characters</Header>
+      <br />
+      <Card.Group>
+        {renderPeople()}
+      </Card.Group>
     </Container>
-  );
+  )
 }
 
 export default App;
