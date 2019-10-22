90ad307d3dc1f386e40db8f55528a3d120f285a2
Michael Kroon <kroonology@gmail.com>
2019-10-22 10:48:33 -0600

    Revert "turn App.js into a class"
    This reverts commit 9d33129be32d332f1d3846a60a0a1ddd94acb15c.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Container, Header } from "semantic-ui-react";
 
-class App extends React.Component {
-  render() {
+function App() {
   return (
     <Container>
       <Header>Hello</Header>
     </Container>
   );
-  }
 }
 
 export default App;
